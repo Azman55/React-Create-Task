29b60bf88ca59f6d9d53398008932d785b1afc4d
Azman <149482156+Azman55@users.noreply.github.com>
2024-03-11 16:27:02 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import TaskInput from './Components/TaskInput';
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="w-full h-[80vh] flex justify-center items-center "> 
+      <TaskInput />
+    </div>
+  )
+  }
+
+export default App;
